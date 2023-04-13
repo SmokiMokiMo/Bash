d836fa8d94a41f7fd97624849f2edb59ce95b8aa
SmokiMokiMo <120665259+SmokiMokiMo@users.noreply.github.com>
2023-04-13 18:47:45 +0300

    Update block_SG.sh

--- block_SG.sh
+++ block_SG.sh
@@ -3,15 +3,13 @@
 echo "Block all Demo SG...with values 'TRUE'"
 
 hostnames=${1}
-
+# Use the IP address in another command
 for hostname in "${hostnames[@]}"
 do
     sudo iptables -I INPUT -p tcp -j DROP -s $ip
     echo "The IP address for $hostname is $ip"
         
-    # Use the IP address in another command
-    # For example:
-    # curl "http://$ip:80"
+   
 done
 echo "-----------------------------------------------------------"
 echo "               ALL SG is Blocked!!!!!!!!!!"
